e82cfe788273d5164a3171ad29c19669d791172e
Crillin222 <iuricasagrande@gmail.com>
2022-04-01 03:08:04 -0300

    Passando os trabalhos para o github

---
+++ Javascript/1/aula.js
@@ -0,0 +1,30 @@
+//Java script não permite espaço entre linhas.
+//Sempre começar as variaveis e constantes com minusculo
+//Variaveis (valores variaveis).
+let idade = 5;
+console.log(idade);
+let altura = 180;
+altura = 175;
+console.log(altura);
+let nomeCompleto = 'Ronaldo Santos';
+console.log(nomeCompleto);
+//Constantes (não redefine o valor).
+const valorIngressoAdulto = 20;
+console.log(valorIngressoAdulto);
+//Tipos Primitivos (Referencia).
+let nome = 'Rafael'; //String literal.
+let idade1 = 25; //Number literal.
+let estaAprovado = true; //Boolean.
+let sobrenome ; //Undefined.
+let corSelecionada = null; //Redefinir um valor.
+//Objetos (Junta informações em um lugar só).
+let pessoa = {
+    nome: 'Rafael',
+    idade1: 25,
+    estaAprovado: true,
+    sobrenome: 'de Souza',
+    altura: 175,
+    
+};
+//
+console.log(pessoa);
